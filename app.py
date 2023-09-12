014336b73e476324b44ed9de632a28a2986f90d0
Chris Alexiuk <c.s.alexiuk@gmail.com>
2023-09-12 12:00:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,42 +4,71 @@
 
 import openai #importing openai for API usage
 import chainlit as cl #importing chainlit for our app
+from chainlit.input_widget import Select, Switch, Slider #importing chainlit settings selection tools
+from chainlit.prompt import Prompt, PromptMessage #importing prompt tools 
+from chainlit.playground.providers import ChatOpenAI #importing ChatOpenAI tools
 
 # You only need the api key inserted here if it's not in your .env file
 #openai.api_key = "YOUR_API_KEY"
 
-# We select our model. If you do not have access to GPT-4, please use GPT-3.5T (gpt-3.5-turbo)
+# ChatOpenAI Templates
+system_template = """You are a helpful assistant who always speaks in a pleasant tone!
+"""
 
-model_name = "gpt-3.5-turbo"
-# model_name = "gpt-4"
-settings = {
-    "temperature": 0.7, # higher value increases output diveresity/randomness
-    "max_tokens": 500, # maximum length of output response
-    "top_p": 1, # choose only the top x% of possible words to return
-    "frequency_penalty": 0, # higher value will result in the model being more conservative in its use of repeated tokens.
-    "presence_penalty": 0, # higher value will result in the model being more likely to generate tokens that have not yet been included in the generated text
-}
+user_template = """{input}
+Think through your response step by step.
+"""
 
 @cl.on_chat_start # marks a function that will be executed at the start of a user session
-def start_chat():
-    cl.user_session.set(
-        "message_history",
-        [{"role": "system", "content": "You are a helpful assistant."}],
-    )
+async def start_chat():
+    settings = {
+        "model": "gpt-3.5-turbo",
+        "temperature": 0,
+        "max_tokens": 500,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+    }
 
+    cl.user_session.set("settings", settings)
 
 @cl.on_message # marks a function that should be run each time the chatbot receives a message from a user
 async def main(message: str):
-    message_history = cl.user_session.get("message_history")
-    message_history.append({"role": "user", "content": message + " Think through your response step by step."})
+
+    settings = cl.user_session.get("settings")
+
+    prompt = Prompt(
+        provider=ChatOpenAI.id,
+        messages=[
+            PromptMessage(
+                role="system",
+                template=system_template,
+                formatted=system_template,
+            ),
+            PromptMessage(
+                role="user",
+                template=user_template, 
+                formatted=user_template.format(input=message),
+            )
+        ],
+        inputs = {"input" : message},
+        settings=settings
+    )
+
+    print([m.to_openai() for m in prompt.messages])
 
     msg = cl.Message(content="")
 
+    # Call OpenAI
     async for stream_resp in await openai.ChatCompletion.acreate(
-        model=model_name, messages=message_history, stream=True, **settings
+        messages=[m.to_openai() for m in prompt.messages], stream=True, **settings
     ):
         token = stream_resp.choices[0]["delta"].get("content", "")
         await msg.stream_token(token)
 
-    message_history.append({"role": "assistant", "content": msg.content})
+    # Update the prompt object with the completion
+    prompt.completion = msg.content
+    msg.prompt = prompt
+
+    # Send and close the message stream
     await msg.send()
